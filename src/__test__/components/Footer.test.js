b8c2af40a21c7186f05f43cd35a54b5e4264c75a
Santiago Ortiz Guevara <santiagortizgue@MacBook-Pro-de-Santiago.local>
2021-08-20 15:19:14 -0500

    simulates fn

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import {
+    mount
+} from 'enzyme';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
-    const footer = mount(<Footer />);
-    
+    const footer = mount( < Footer / > );
+
     test('Render del componente Footer', () => {
         expect(footer.length).toEqual(1);
     });
@@ -12,4 +14,8 @@ describe('<Footer />', () => {
     test('Render del titulo', () => {
         expect(footer.find(".Footer-title").text()).toEqual("Platzi Store");
     });
+});
+
+describe('Footer Snapshot', () => {
+    
 });
